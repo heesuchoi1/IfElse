11b508b84c882fb013196a610e6a1abc275c809d
heesuchoi1 <heesu7563@gmail.com>
2014-10-02 16:57:10 +0900

    da

--- sketch.js
+++ sketch.js
@@ -1,16 +1,41 @@
 function setup(){
 createCanvas(680,600)
 noFill();
-stroke();
+stroke(255,255,255);
 }
 
 function draw(){
 	background(200,200,200);
-	ellipse(50,50,mouseX,mouseY);
 
-	if(mouseX < 300) {
-		ellipse(200,200,100,100);
-	} else{
-		ellipse(400,200,100,100);
+	if(mouseX < 100) {
+		fill(255,255,255);
+		ellipse(50,200,100,100);
+	}
+
+	else if (mouseX < 200){
+
+		fill(146,240,255);
+		ellipse(200,200,100,100)
+		rect(150,200,100,100);
+	}
+
+	else if (mouseX < 300){
+		fill(0,195,255);
+		ellipse(250,200,100,100);
+	}
+
+	else if (mouseX < 400){
+		fill(0,175,216);
+		ellipse(350,200,100,100);
+	}
+
+	else if (mouseX < 500){
+		fill(0,175,33);
+		ellipse(450,200,100,100);
+	}
+
+	else{
+		ellipse(550,200,100,100);
+		ellipse(550,200,50,50);
 	}
 }
