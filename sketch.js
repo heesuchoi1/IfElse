968d3ac9b931c9162a1058a942b73b03f0073fcf
heesuchoi1 <heesu7563@gmail.com>
2014-10-02 16:23:49 +0900

    ffffffdfgdrgergergerg

--- sketch.js
+++ sketch.js
@@ -6,5 +6,11 @@ stroke();
 
 function draw(){
 	background(200,200,200);
-	ellipse(mouseX,mouseY,50,50);
+	ellipse(50,50,mouseX,mouseY);
+
+	if(mouseX < 300) {
+		ellipse(200,200,100,100);
+	} else{
+		ellipse(400,200,100,100);
+	}
 }
